aca097821ea833c9e521c11036bb88bec6d1bfb1
Jitendar Singh <jsingh@localhost.localdomain>
2020-02-27 21:48:21 +0530

    Updated features file

--- features/steps/steps.py
+++ features/steps/steps.py
@@ -315,6 +315,35 @@ def step_impl(context):
 @when(u'we run \'oc new-app jenkins-persistent\' on the terminal')
 def step_impl(context):
     raise NotImplementedError(u'STEP: When we run \'oc new-app jenkins-persistent\' on the terminal')
+@given(u'I have a project')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Given I have a project')
+
+
+@when(u'I run the :new_app client command with')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I run the :new_app client command with')
+
+
+@then(u'the step should succeed')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then the step should succeed')
+
+
+@when(u'I execute on the pod')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I execute on the pod')
+
+
+@then(u'the output should contain')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then the output should contain')
+
+
+@then(u'a pod becomes ready with labels')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then a pod becomes ready with labels')
+
 
 
 
